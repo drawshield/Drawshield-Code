9c9b9b3b2ada9e10fddebb476dfd652d53c168d1
Mattia Basaglia <glax@dragon.best>
2021-03-19 22:20:09 +0100

    Adjust in annulo and chief height

--- shieldcommon.js
+++ shieldcommon.js
@@ -423,7 +423,8 @@ function format_blazon_ml(element, target_id)
         {
             if ( child instanceof Element )
             {
-                format_impl(target, child, indent+1);
+                if ( child.tagName != "blazonML:input" )
+                    format_impl(target, child, indent+1);
             }
             else if ( child instanceof Text )
             {
