72caacb39a4c0bce98c68eca326a199489ab4417
Mattia Basaglia <glax@dragon.best>
2021-03-28 16:57:57 +0200

    Load edge types from file

--- svg/bezier_test.php
+++ svg/bezier_test.php
@@ -9,6 +9,7 @@ register_shutdown_function(function()
 
 require_once("bezier.inc");
 require_once("shapes.inc");
+require_once("edges/edge_type.inc");
 
 $edge_path_embattled = new PolyBezier;
 $edge_path_embattled
@@ -46,12 +47,13 @@ $edge_path_flory
     ->line_to(new ShieldPos(5, 10))
     ->line_to(new ShieldPos(10, 0))
 ;
-$edge = new EdgeType($edge_path_wavy);
+// $edge = new EdgeType($edge_path_wavy);
 // $edge = new EdgeTypeFlory($edge_path_flory);
-$bez = SvgDParser::parse_d(getShape(new ShieldLayout(new ShieldSize(1, 1), "french")));
-// $bez->sub_paths()[0]->segment_tags[0] = BezierSegmentFlags::NORMAL;
-// $bez->sub_paths()[0]->segment_tags[5] = BezierSegmentFlags::NORMAL;
-// $bez->sub_paths()[0]->segment_tags[10] = BezierSegmentFlags::NORMAL;
+$edge = EdgeType::load("dancetty-floretty");
+$bez = SvgDParser::parse_d(getShape(new ShieldLayout(new ShieldSize(1, 1), "heater")));
+$bez->sub_paths()[0]->segment_tags[0] = BezierSegmentFlags::NORMAL;
+$bez->sub_paths()[0]->segment_tags[5] = BezierSegmentFlags::NORMAL;
+$bez->sub_paths()[0]->segment_tags[10] = BezierSegmentFlags::NORMAL;
 /*
 $bez1 = clone $bez;
 $bez1->scale(0.5);
@@ -59,16 +61,11 @@ $bez1->translate(new ShieldPos(250, 250));
 $bez1->reverse();
 $bez->combine_from($bez1);
 */
-$comp = $bez->rounded(300)->compile();
-
-$p1 = new ShieldPos(400, 50);
-$p2 = new ShieldPos(300, 120);
-$edge_test = new MutableShape();
-$edge_test->move_to($p1);
-$edge->apply_line_segment($p1, $p2, $edge_test);
+$comp = $bez->compile();
 
 $modified = new MutableShape();
-$edge->apply($comp, $modified, 200, 200);
+$edge->apply($comp, $modified, 100, 100);
+
 
 
 header("content-type: image/svg+xml");
@@ -85,9 +82,18 @@ echo "<path d='" . $comp->to_svg_d() . "' fill='blue' />";
 echo "<path d='" . $modified->to_svg_d() . "' fill='none' stroke='orange' stroke-width='4' />";
 echo "</g>";
 
-echo "<path d='" . $edge->shape->to_svg_d() . "' fill='none' stroke-width='0.01' stroke='orange' transform='translate(50, 50) scale(100, 100)' />";
-echo "<path d='M {$p1->x} {$p1->y} {$p2->x} {$p2->y}' fill='none' stroke='red' />";
-echo "<path d='" . $edge_test->to_svg_d() . "' fill='none' stroke='orange' />";
+if ( $edge instanceof EdgeTypeBezier )
+{
+    $p1 = new ShieldPos(400, 50);
+    $p2 = new ShieldPos(300, 120);
+    $edge_test = new MutableShape();
+    $edge_test->move_to($p1);
+    $edge->apply_line_segment($p1, $p2, $edge_test);
+
+    echo "<path d='" . $edge->shape->to_svg_d() . "' fill='none' stroke-width='0.01' stroke='orange' transform='translate(50, 50) scale(100, 100)' />";
+    echo "<path d='M {$p1->x} {$p1->y} {$p2->x} {$p2->y}' fill='none' stroke='red' />";
+    echo "<path d='" . $edge_test->to_svg_d() . "' fill='none' stroke='orange' />";
+}
 
 echo "\n</svg>\n";
 
