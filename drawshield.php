3c82ae4c6342d958d6284942d43833ea2ebe10f7
Karl Wilcox <karl@karlwilcox.com>
2023-10-05 22:53:49 +0100

    Changes to support easier remote calls, especially JSON format.

--- drawshield.php
+++ drawshield.php
@@ -18,6 +18,7 @@ This file is part of the DrawShield.net heraldry image creation program
 
 include 'utils/general.inc';  // general utilities, duh.
 $timings = array('start' => microtime(true));
+$memory = array('start' => memory_get_usage(true));
 
 //////////////////////////////////////////////
 // Stage 1a - Set default and fallback options
@@ -40,7 +41,7 @@ if (isset($argc)) {
             elseif ($option == 'whcols') $options['useWarhammerColours'] = $value == 'yes';
             else $options[$option] = $value;
         }
-        $options['commandLine'] = true;
+        $options['requestSource'] = 'shell';
         if (!array_key_exists('blazon', $options))
             $options['blazon'] = implode(' ', $myArgs['arguments']);
     } else { // otherwise, run anything in the debug script
@@ -56,30 +57,17 @@ if (isset($argc)) {
 $ar = null;
 $request = array_merge($_GET, $_POST);
 $svgOutput = null;
+$dom = null;
+$messages = null;
+$jsonData = [];
 
-// We do support sending the blazon in an uploaded file but this is not currently implemented
-// on the site anywhere, we always use POST fields
-if (isset($_FILES['blazonfile']) && ($_FILES['blazonfile']['name'] != "")) {
-    $fileName = $_FILES['blazonfile']['name'];
-    $fileSize = $_FILES['blazonfile']['size'];
-    $fileTmpName = $_FILES['blazonfile']['tmp_name'];
-    // $fileType = $_FILES['blazonfile']['type']; // not currently used
-    if (preg_match('/.txt$/i', $fileName) && $fileSize < 1000000) {
-        $options['blazon'] = file_get_contents($fileTmpName);
-    }
-} else {
-    if (isset($request['blazon'])) $options['blazon'] = strip_tags($request['blazon']);
-}
 
 // Process superglobal settings into the options array
 if (isset($request['outputformat'])) $options['outputFormat'] = strip_tags($request['outputformat']);
 if (isset($request['saveformat'])) $options['saveFormat'] = strip_tags($request['saveformat']);
 if (isset($request['asfile']) && $request['asfile'] != '0') $options['asFile'] = strip_tags($request['asfile']);
 if (isset($request['palette'])) $options['palette'] = strip_tags($request['palette']);
-if (isset($request['shape'])) {
-    $options['shape'] = strip_tags($request['shape']);
-    $options['shapeSet'] = true;
-}
+if (isset($request['shape'])) $options['shape'] = strip_tags($request['shape']);
 if (isset($request['stage'])) $options['stage'] = strip_tags($request['stage']);
 if (isset($request['filename'])) $options['filename'] = strip_tags($request['filename']);
 if (isset($request['effect'])) $options['effect'] = strip_tags($request['effect']);
@@ -90,100 +78,120 @@ if (isset($request['webcols'])) $options['useWebColours'] = $request['webcols'] 
 if (isset($request['tartancols'])) $options['useTartanColours'] = $request['tartancols'] == 'yes';
 if (isset($request['whcols'])) $options['useWarhammerColours'] = $request['whcols'] == 'yes';
 if (isset($request['customPalette']) && is_array($request['customPalette'])) $options['customPalette'] = $request['customPalette'];
-// We might be given the SVG to convert to another format, so do a basic sense check
-if (isset($request['svg']) && strlen($request['svg']) > 20) $svgOutput = $request['svg'];
 
+//////////////////////////////////////////////
+// Stage 2a - blazon preparation
+/////////////////////////////////////////////
 
-if (is_null($svgOutput)) { // not given SVG, so generate it from the blazon
-
-    //////////////////////////////////////////////
-    // Stage 2a - blazon preparation
-    /////////////////////////////////////////////
-
-    $dom = null;
-    // If blazon is null assume 'argent', but skip parsing as we already know the outcome
-    if ($options['blazon'] == '') {
-        $dom = new DOMDocument('1.0');
-        $dom->loadXML($version['dummyAST']);
-    } else {
-        $options['blazon'] = preg_replace("/&#?[a-z0-9]{2,8};/i", "", $options['blazon']); // strip all entities.
-        $options['blazon'] = preg_replace("/\\x[0-9-a-f]{2}/i", "", $options['blazon']); // strip all entities.
-        // log the blazon for research... (unless told not too)
-        if ($options['logBlazon']) error_log($options['blazon']);
+// We do support sending the blazon in an uploaded file but this is not currently implemented
+// on the site anywhere, we always use POST fields
+if (isset($_FILES['blazonfile']) && ($_FILES['blazonfile']['name'] != "")) {
+    $fileName = $_FILES['blazonfile']['name'];
+    $fileSize = $_FILES['blazonfile']['size'];
+    $fileTmpName = $_FILES['blazonfile']['tmp_name'];
+    // $fileType = $_FILES['blazonfile']['type']; // not currently used
+    if (preg_match('/.txt$/i', $fileName) && $fileSize < 1000000) {
+        $options['blazon'] = file_get_contents($fileTmpName);
+        $options['requestSource'] = 'upload';
     }
-
-
-    include 'parser/utilities.inc';
-    include "analyser/utilities.inc";
-    if (is_null($dom)) {
-        //////////////////////////////////////////////
-        // Stage 2b - Parsing into XML AST
-        /////////////////////////////////////////////
-        include "parser/parser.inc";
-
-        $p = new parser('english');
-        $dom = $p->parse($options['blazon'], 'dom');
-        $p = null; // destroy parser to save memory
-        $timings['parser'] = microtime(true);
-
-        //////////////////////////////////////////////
-        // Stage 2c - Filtering out stuff we don't like (code not in repo)
-        /////////////////////////////////////////////
-        if (file_exists("/opt/bitnami/apache/etc/filter.inc")) {
-            include "/opt/bitnami/apache/etc/filter.inc";
-            $filter = new filter($dom);
-            $dom = $filter->runFilter();
-        }
-
-        //////////////////////////////////////////////
-        // Stage 2d - Additional Options may have been in blazon, tidy them up
-        /////////////////////////////////////////////
-        if ($options['asFile']) {
-            $options['printSize'] = $options['size'];
-            $options['size'] = 1000;
-        }
-        // Minimum sensible size
-        if ($options['size'] < 100) $options['size'] = 100;
-        if ($ar != null) {
-            $options['aspectRatio'] = calculateAR($ar);
-        }
-        tidyOptions();;
-
-
-        //////////////////////////////////////////////
-        // Stage 2e - resolve cross references & other fixups
-        /////////////////////////////////////////////
-        include "analyser/references.inc";
-        $references = new references($dom);
-        $dom = $references->setReferences();
-        $references = null; // destroy references to save memory
-        $timings['fixups'] = microtime(true);
-    }
-
-
-    //////////////////////////////////////////////
-    // Stage 3a - preparation for drawing
-    /////////////////////////////////////////////
-    $xpath = new DOMXPath($dom);
-    include 'utils/messages.inc';
-    $messages = new messageStore($dom);
-    include "svg/draw.inc";
-    set_error_handler("report_errors_svg");
-
-
-    //////////////////////////////////////////////
-    // Stage 3b - create SVG version (all formats start as SVG)
-    /////////////////////////////////////////////
-    $svgOutput = draw();
+} else {
+    if (isset($request['blazon'])) $options['blazon'] = $request['blazon'];
 }
+
+// If blazon is null assume 'argent', but skip parsing as we already know the outcome
+if ($options['blazon'] == '') {
+    $dom = new DOMDocument('1.0');
+    $dom->loadXML($version['dummyAST']);
+} else {
+    if ($options['outputFormat'] == 'json') {
+        // start gathering our parts here
+        $jsonData['blazon'] = $options['blazon'];
+        $jsonData['creationTime'] = date(DATE_RFC2822);
+        $jsonData['pathInfo'] = $_SERVER['REQUEST_URI'] ?? '???';
+        $jsonData['options'] = $options;
+        unset($jsonData['options']['blazon']); // already have this
+    }
+    $options['blazon'] = strip_tags($options['blazon']);
+    $options['blazon'] = preg_replace("/&#?[a-z0-9]{2,8};/i", "", $options['blazon']); // strip all entities.
+    $options['blazon'] = preg_replace("/\\x[0-9-a-f]{2}/i", "", $options['blazon']); // strip all entities.
+    // log the blazon for research... (unless told not too)
+    if ($version['logBlazon']) error_log($options['blazon']);
+}
+
+
+include 'parser/utilities.inc';
+include "analyser/utilities.inc";
+if (is_null($dom)) {
+    //////////////////////////////////////////////
+    // Stage 2b - Parsing into XML AST
+    /////////////////////////////////////////////
+    include "parser/parser.inc";
+
+    $p = new parser('english');
+    $dom = $p->parse($options['blazon'], 'dom');
+    $memory['parser'] = memory_get_usage(true);
+    $p = null; // destroy parser to save memory
+    $timings['parser'] = microtime(true);
+
+    //////////////////////////////////////////////
+    // Stage 2c - Filtering out stuff we don't like (code not in repo)
+    /////////////////////////////////////////////
+    if (file_exists("/opt/bitnami/apache/etc/filter.inc")) {
+        include "/opt/bitnami/apache/etc/filter.inc";
+        $filter = new filter($dom);
+        $dom = $filter->runFilter();
+    }
+
+    //////////////////////////////////////////////
+    // Stage 2d - Additional Options may have been in blazon, tidy them up
+    /////////////////////////////////////////////
+    if ($options['asFile']) {
+        $options['printSize'] = $options['size'];
+        $options['size'] = 1000;
+    }
+    // Minimum sensible size
+    if ($options['size'] < 100) $options['size'] = 100;
+    if ($ar != null) {
+        $options['aspectRatio'] = calculateAR($ar);
+    }
+    tidyOptions();;
+
+
+    //////////////////////////////////////////////
+    // Stage 2e - resolve cross references & other fixups
+    /////////////////////////////////////////////
+    include "analyser/references.inc";
+    $references = new references($dom);
+    $dom = $references->setReferences();
+    $memory['fixups'] = memory_get_usage(true);
+    $references = null; // destroy references to save memory
+    $timings['fixups'] = microtime(true);
+}
+
+
+//////////////////////////////////////////////
+// Stage 3a - preparation for drawing
+/////////////////////////////////////////////
+$xpath = new DOMXPath($dom);
+include 'utils/messages.inc';
+$messages = new messageStore($dom);
+include "svg/draw.inc";
+set_error_handler("report_errors_svg");
+
+
+//////////////////////////////////////////////
+// Stage 3b - create SVG version (all formats start as SVG)
+/////////////////////////////////////////////
+$svgOutput = draw();
+$memory['svg draw'] = memory_get_usage(true);
 
 // At this point we should have some valid SVG in $svgOutput, now what?
 $target = $options['outputFormat'];
 if ($target == 'json') {
-    $target = 'png';
+    $target = $options['saveFormat'];
 } elseif ($options['asFile'] == 'printable') {
     $target = 'svg';
-} elseif ($options['asFile'] || $options['commandLine']) {
+} elseif ($options['asFile']) {
     $target = $options['saveFormat'];
 }
 // (Sorry about the above mess, needed for historic reasons and to preserve API arguments)
@@ -199,6 +207,11 @@ if ($target == 'svg') {
     include 'utils/render.inc';
     $targetImage = convertImageFormat($svgOutput, $target);
     $timings['conversion'] = microtime(true);
+    $memory['conversion'] = memory_get_usage(true);
+}
+
+if ($options['outputFormat'] == 'json') { // package up things for output as needed
+    $targetImage = makeJsonArray(true);
 }
 
 //////////////////////////////////////////////
@@ -221,65 +234,17 @@ if ($options['asFile'] == 'printable') {
     echo $messages->getCredits();
     echo "</body>\n</html>\n";
     //////////////////////////////////////////////
-    // Stage 4 option 2 - An image file downloaded over HTTP
+    // Stage 4 option 2 - file saved locally in file system
     /////////////////////////////////////////////
-} elseif ($options['asFile']) {
-    $name = $options['filename'];
-    if ($name == '') $name = 'shield';
-    switch ($options['saveFormat']) {
-        case 'svg':
-            if (substr($name, -4) != '.svg') $name .= '.svg';
-            header("Content-type: application/force-download");
-            header('Content-Disposition: inline; filename="' . $name);
-            header("Content-Transfer-Encoding: text");
-            header('Content-Disposition: attachment; filename="' . $name);
-            header('Content-Type: image/svg+xml');
-            break;
-        case 'pdfLtr':
-        case 'pdfA4':
-            if (substr($name, -4) != '.pdf') $name .= '.pdf';
-            header("Content-type: application/force-download");
-            header('Content-Disposition: inline; filename="' . $name);
-            header("Content-Transfer-Encoding: 8bit");
-            header('Content-Disposition: attachment; filename="' . $name);
-            header('Content-Type: application/pdf');
-            break;
-        case 'jpg':
-        case 'jpeg':
-            if (substr($name, -4) != '.jpg') $name .= '.jpg';
-            header("Content-type: application/force-download");
-            header('Content-Disposition: inline; filename="' . $name);
-            header("Content-Transfer-Encoding: binary");
-            header('Content-Disposition: attachment; filename="' . $name);
-            header('Content-Type: image/jpg');
-            break;
-        case 'png':
-            if (substr($name, -4) != '.png') $name .= '.png';
-            header("Content-type: application/force-download");
-            header('Content-Disposition: inline; filename="' . $name);
-            header("Content-Transfer-Encoding: binary");
-            header('Content-Disposition: attachment; filename="' . $name);
-            header('Content-Type: image/png');
-            break;
-        case 'webp':
-            if (substr($name, -4) != '.webp') $name .= '.webp';
-            header("Content-type: application/force-download");
-            header('Content-Disposition: inline; filename="' . $name);
-            header("Content-Transfer-Encoding: binary");
-            header('Content-Disposition: attachment; filename="' . $name);
-            header('Content-Type: image/webp');
-            break;
-    }
-    echo $targetImage;
-    //////////////////////////////////////////////
-    // Stage 4 option 3 - file saved locally in file system
-    /////////////////////////////////////////////
-} elseif ($options['commandLine']) {
-    // This only occurs if the file is run locally, and we don't specify "asFile" (i.e. HTTP download). We write the file locally.
+} elseif ($options['requestSource'] == 'shell') {
+    // We always just write the file locally.
     $name = $options['filename'];
     if ($name == '') $name = 'shield';
     $fileContent = '';
     switch ($options['saveFormat']) {
+        case 'json':
+            if (substr($name, -5) != '.json') $name .= '.json';
+            break;
         case 'svg':
             if (substr($name, -4) != '.svg') $name .= '.svg';
             break;
@@ -297,6 +262,62 @@ if ($options['asFile'] == 'printable') {
     }
     file_put_contents($name, $targetImage);
     //////////////////////////////////////////////
+    // Stage 4 option 3 - An image file downloaded over HTTP
+    /////////////////////////////////////////////
+} elseif ($options['asFile']) {
+    if ($options['outputFormat'] == 'json') { // override saveFormat
+        $options['saveFormat'] = 'json';
+    }
+    $name = $options['filename'];
+    if ($name == '') $name = 'shield';
+    switch ($options['saveFormat']) {
+        case 'json':
+            if (substr($name, -5) != '.json') $name .= '.json';
+            header("Content-type: application/force-download");
+            header("Content-Transfer-Encoding: text");
+            header('Content-Disposition: attachment; filename="' . $name .'"');
+            header('Content-Type: application/json');
+            break;
+        case 'svg':
+            if (substr($name, -4) != '.svg') $name .= '.svg';
+            header("Content-type: application/force-download");
+            header("Content-Transfer-Encoding: text");
+            header('Content-Disposition: attachment; filename="' . $name .'"');
+            header('Content-Type: image/svg+xml');
+            break;
+        case 'pdfLtr':
+        case 'pdfA4':
+            if (substr($name, -4) != '.pdf') $name .= '.pdf';
+            header("Content-type: application/force-download");
+            header("Content-Transfer-Encoding: 8bit");
+            header('Content-Disposition: attachment; filename="' . $name .'"');
+            header('Content-Type: application/pdf');
+            break;
+        case 'jpg':
+        case 'jpeg':
+            if (substr($name, -4) != '.jpg') $name .= '.jpg';
+            header("Content-type: application/force-download");
+            header("Content-Transfer-Encoding: binary");
+            header('Content-Disposition: attachment; filename="' . $name .'"');
+            header('Content-Type: image/jpg');
+            break;
+        case 'png':
+            if (substr($name, -4) != '.png') $name .= '.png';
+            header("Content-type: application/force-download");
+            header("Content-Transfer-Encoding: binary");
+            header('Content-Disposition: attachment; filename="' . $name .'"');
+            header('Content-Type: image/png');
+            break;
+        case 'webp':
+            if (substr($name, -4) != '.webp') $name .= '.webp';
+            header("Content-type: application/force-download");
+            header("Content-Transfer-Encoding: binary");
+            header('Content-Disposition: attachment; filename="' . $name .'"');
+            header('Content-Type: image/webp');
+            break;
+    }
+    echo $targetImage;
+    //////////////////////////////////////////////
     // Stage 4 option 4 - image returned over HTTP
     /////////////////////////////////////////////
 } else {
@@ -305,36 +326,7 @@ if ($options['asFile'] == 'printable') {
             header('Content-Type: image/jpg');
             break;
         case 'json': // A PNG image + other data all wrapped up in JSON
-            $newDom = new DOMDocument();
-            $newDom->loadXML($svgOutput);
-            $json = [];
-            $json['image'] = base64_encode($targetImage);
-            $json['options'] = $options;
-            $allMessages = $newDom->getElementsByTagNameNS('http://drawshield.net/blazonML', 'message');
-            $messageArray = [];
-            foreach ($allMessages as $node) {
-                $thisMessage = [];
-                for ($i = 0; $i < $node->attributes->length; $i++) {
-                    $thisMessage[$node->attributes->item($i)->nodeName] = $node->attributes->item($i)->nodeValue;
-                }
-                $thisMessage['content'] = $node->nodeValue;
-                $messageArray[] = $thisMessage;
-            }
-            $json['messages'] = $messageArray;
-            $json['tree'] = $dom->saveXML();
-            $baggage = $dom->getElementsByTagNameNS('http://drawshield.net/blazonML', 'input')->item(0);
-            $baggage->parentNode->removeChild($baggage);
-            $baggage = $dom->getElementsByTagNameNS('http://drawshield.net/blazonML', 'messages')->item(0);
-            $baggage->parentNode->removeChild($baggage);
-            $minTree = $dom->saveXML();
-            $minTree = preg_replace('/blazonML:/', '', $minTree);
-            $minTree = preg_replace('/<\?xml.*\?>\n/', '', $minTree);
-            $minTree = preg_replace('/<\/?blazon.*>\n/', '', $minTree);
-            $minTree = preg_replace('/[<>"]/', '', $minTree);
-            $json['mintree'] = $minTree;
-            $json['timings'] = showTimings();
             header('Content-Type: application/json');
-            $targetImage = json_encode($json);
             break;
         case 'webp':
             header('Content-Type: image/webp');
